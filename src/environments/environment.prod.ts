1799f7b890c104b198f6862fe32decd21e99cd9f
Gustavo Huerta <g.huerta01@ufromail.cl>
2020-03-30 05:38:27 +0200

    change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api: 'http://ec2-3-14-14-67.us-east-2.compute.amazonaws.com:8080'
+  api: 'https://adminio-backend.herokuapp.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  api: 'http://ec2-3-14-14-67.us-east-2.compute.amazonaws.com:8080',
+  api: 'https://adminio-backend.herokuapp.com',
   firebase: {
     apiKey: "AIzaSyCJzZDVH5BLq3S4jq6z_lB4WSrUpyi0k2Y",
     authDomain: "adminio.firebaseapp.com",
