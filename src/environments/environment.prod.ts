73c199d2fea732b63ea57751d8ffc37364b6bd66
Gustavo Huerta <g.huerta01@ufromail.cl>
2020-03-30 23:22:49 +0200

    add firebase property to environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,14 @@
 export const environment = {
   production: true,
-  api: 'https://adminio-backend.herokuapp.com'
+  api: 'https://adminio-backend.herokuapp.com',
+  firebase: {
+    apiKey: "AIzaSyCJzZDVH5BLq3S4jq6z_lB4WSrUpyi0k2Y",
+    authDomain: "adminio.firebaseapp.com",
+    databaseURL: "https://adminio.firebaseio.com",
+    projectId: "adminio",
+    storageBucket: "adminio.appspot.com",
+    messagingSenderId: "490867024093",
+    appId: "1:490867024093:web:11e9723c8cafa1ac85bb34",
+    measurementId: "G-KEZ98PLV9X"
+  }
 };
